34df3b512e571148ddef457e62e15445604c6523
Zeynep ALAKA <56490184+zeynepalaka@users.noreply.github.com>
2022-10-30 23:16:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapmakine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int res;
+                Console.Write("Birinci Sayıyı Giriniz:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("İkinci Sayıyı Giriniz:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Sembol Giriniz:(/,+,-,*):");
+                string symbol = Console.ReadLine();
+
+                switch (symbol)
+                {
+                    case "+":
+                        res = num1 + num2;
+                        Console.WriteLine("Toplam:" + res);
+                        break;
+                    case "-":
+                        res = num1 - num2;
+                        Console.WriteLine("Çıkarma:" + res);
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        Console.WriteLine("Çarpma:" + res);
+                        break;
+                    case "/":
+                        res = num1 / num2;
+                        Console.WriteLine("Bölme:" + res);
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış Giriş!!!");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Write("Devam etmek istiyor musun(e/h):");
+                value = Console.ReadLine();
+            }
+            while (value == "e" || value == "E");
+        }
+    }
+
+}
